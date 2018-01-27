e03c41fd0cdbf1d7cca50d9d047623a0e49ff3bd
db001 <dnbennett@hotmail.co.uk>
2018-01-27 22:21:24 +0000

    Finished animation

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,7 +1,9 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-  /* removed */
+  apiKey: "AIzaSyCfMFIzl9rD1v6dFaEpn05cGf8qvw__QEA",
+  authDomain: "catchofthedaydb.firebaseapp.com",
+  databaseURL: "https://catchofthedaydb.firebaseio.com",
 });
 
 export default base;
